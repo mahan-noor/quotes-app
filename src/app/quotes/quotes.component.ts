e030e0753b9a138b6772f6826fa646e289bfe79a
mahan-noor <ismahanabey@gmail.com>
2021-03-26 17:10:28 +0300

    attach the array of quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes:Quotes[] =[
+    {id:1,John:'Watch finding Nemo'},
+    {id:2,Amina:'If you really look closely, most overnight successes took a long time.'},
+    {id:3,Ilhan:'The future belongs to those who believe in the beauty of their dreams.'},
+    {id:4,Cynthia:'Things work out best for those who make the best of how things work out.'},
+    {id:5,Savannah:'Try not to become a man of success. Rather become a man of value'},
+    {id:6,Mariam:'Dont be afraid to give up the good to go for the great.'},
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
